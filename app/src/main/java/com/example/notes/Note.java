9bd525aec281ed7de3cbd4c4124860a055ad959a
Instinktaz <tomas.paskovskij@gmail.com>
2019-07-21 22:08:27 +0300

    add Notes project

---
+++ app/src/main/java/com/example/notes/Note.java
@@ -0,0 +1,69 @@
+package com.example.notes;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+import com.google.common.collect.Table;
+
+@Entity(tableName = "notes")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+    private String dayOfWeek;
+    private int priority;
+
+    public Note(int id, String title, String description, String dayOfWeek, int priority) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dayOfWeek = dayOfWeek;
+        this.priority = priority;
+    }
+
+    @Ignore
+    public Note(String title, String description, String dayOfWeek, int priority) {
+        this.title = title;
+        this.description = description;
+        this.dayOfWeek = dayOfWeek;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
